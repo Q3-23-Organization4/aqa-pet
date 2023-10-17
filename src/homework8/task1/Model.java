20284de186f3069c4743dd810558f0728fde2dbc
NikaBuravska <buravska.nika@gmail.com>
2023-10-17 10:47:01 +0300

    added task 8

---
+++ src/homework8/task1/Model.java
@@ -0,0 +1,31 @@
+package homework8.task1;
+
+public class Model extends MobilePhone implements Mobile{
+    protected String phoneNumber = "76856656";
+
+    protected String modelType = "IOS";
+
+    protected String modelYear = "05-01-2021";
+
+    protected String message = "Hello";
+
+    @Override
+    public void mobilePhone() {
+        System.out.println("mobile phone  " + phoneNumber);
+    }
+
+    @Override
+    public void makeCall() {
+        System.out.println("call to " + phoneNumber);
+    }
+
+    @Override
+    public void sendTextMessage() {
+        System.out.println("message sending " + message);
+    }
+
+    public void modelName(){
+        System.out.println("mobile type " + modelType);
+    }
+
+}
---
+++ src/homework8/task2/Book.java
@@ -0,0 +1,26 @@
+package homework8.task2;
+
+public class Book implements Printable{
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    @Override
+    public void print() {
+       System.out.println(title);
+    }
+
+    public static void printBooks(Printable[] printable) {
+        System.out.println("Bool list ");
+        for (Printable item : printable) {
+            if (item instanceof Book) {
+                System.out.println(((Book) item).getTitle());
+            }
+        }
+    }
+}
