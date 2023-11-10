a067a12e980bba6d8bdc67012248de22805f48b6
NikaBuravska <buravska.nika@gmail.com>
2023-11-10 09:58:06 +0200

    fix in secondTask

--- src/homework10/SecondTask.java
+++ src/homework10/SecondTask.java
@@ -7,19 +7,22 @@ public class SecondTask {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
         numbers.add(9);
-        numbers.add(5);
-        numbers.add(95);
+        numbers.add(50000);
+        numbers.add(950);
         numbers.add(0);
         numbers.add(155);
 
         int X = 100;
 
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) > X) {
-                int number = numbers.remove(i);
-                numbers.add(number);
+        numbers.sort((a, b) -> {
+            if (a <= X && b > X) {
+                return -1;
+            } else if (a > X && b <= X) {
+                return 1;
+            } else {
+                return 0;
             }
-        }
+        });
 
         System.out.println("Result " + numbers);
     }
